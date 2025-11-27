3ab51487ff796ddcf199e02a2efb1c4fd907a4ac
WonJin Hong <own1045@gmail.com>
2025-11-27 15:48:18 +0900

    add sh

---
+++ calc_human_pelvis.sh
@@ -0,0 +1,3 @@
+python onlyhumans.py --bg_pcd_path ./data/test0.pcd --human_pcd_path ./data/test1.pcd --out_pcd_path ./data/result/test_result1.pcd
+
+python pred_upperbody.py --human_pcd_path ./data/test_result1.pcd
