a91fc2cf895702467a3227608810f60a6e2fa3e3
BeepingBusyBeaver <twoskysw@gmail.com>
2025-11-27 17:24:21 +0900

    commit

--- calc_human_pelvis.sh
+++ calc_human_pelvis.sh
@@ -1,3 +1,8 @@
-python onlyhumans.py --bg_pcd_path ./data/test0.pcd --human_pcd_path ./data/test1.pcd --out_pcd_path ./data/result/test_result1.pcd
+python calc_humans.py --bg_pcd_path ./data/test0.pcd --human_pcd_path ./data/test1.pcd --out_pcd_path ./data/result/test_result1.pcd
+python calc_upperbody.py --human_pcd_path ./data/test_result1.pcd
 
-python pred_upperbody.py --human_pcd_path ./data/test_result1.pcd
+
+
+python patch_bvh_angles.py ./BVH/input.bvh --Hips --RightCollar --RightShoulder --RightElbow --RightWrist
+python endSite.py ./BVH/input_patched.bvh
+python L2distance_with_csv.py ./BVH/input_patched.bvh
